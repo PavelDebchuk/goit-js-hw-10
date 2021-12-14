493583ce3077765ea146d99d3db64519cd949026
Pavel <Pavel.Debchuk@gmail.com>
2021-12-14 23:14:28 +0300

    дз
    Працює input

--- src/index.js
+++ src/index.js
@@ -4,39 +4,41 @@ import './css/styles.css';
 import countryCardTpl from './card.hbs';
 const DEBOUNCE_DELAY = 300;
 
-// const input = document.querySelector('#search-box');
-
-const countryList = document.querySelector('.country-list');
-
-const countryInfo = document.querySelector('.country-info');
-
+const refs = {
+    cardContainer: document.querySelector('.country-info'),
+};
 
 const nameS = document.querySelector('#search-box');
 
-nameS.addEventListener('input', nameSearch);
+
+
+nameS.addEventListener('input',
+    // _.debounce(() => {
+        nameSearch
+    // }, 1000)
+);
+
+
+
+
 function nameSearch(){
-    
-fetchCountry()
-    .then(renderCountryCard)
-    .catch(error => console.log(error));
-}
-//{name},{capital},{population},{flags},{languages}
-
-
+    let name = nameS.value;
+    fetchCountry()
+        .then(renderCountryCard)
+        .catch(error => console.log(error));
 
 function fetchCountry(){
     
-    let name = nameS.value;
     return fetch(`https://restcountries.com/v2/name/${name}`)
-    .then(response => {
-        return response.json( );
-    })
-    
+    .then(response =>{
+        return response.json();
+    },
+    );
 }
 
+function renderCountryCard(names) {
+    const markup = countryCardTpl(names);
+    refs.cardContainer.innerHTML = markup;
+    }
+}
 
-function renderCountryCard(dataCountry) {
-    console.log(dataCountry);
-    const markup = countryCardTpl(dataCountry);
-    countryInfo.innerHTML = markup;
-} 
