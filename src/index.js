2408e8d72d2bd0e0e6e41a49b63fa8faddde3e7d
Pavel <Pavel.Debchuk@gmail.com>
2021-12-17 01:19:14 +0300

    dz/90%

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './css/styles.css';
 
-
 import countryCardTpl from './card.hbs';
+
+import Notiflix from 'notiflix';
+
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
@@ -9,20 +11,15 @@ const refs = {
 };
 
 const nameS = document.querySelector('#search-box');
-
-
-
-nameS.addEventListener('input',
-    // _.debounce(() => {
-        nameSearch
-    // }, 1000)
-);
-
-
-
-
+nameS.addEventListener('input', _.debounce(nameSearch, 1000));
 function nameSearch(){
-    let name = nameS.value;
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+    let name = _.trim(nameS.value,' ');
+    if(name === '') {
+            let listItem = document.querySelector('.list-item')
+            listItem.style.visibility = "hidden";
+            return;
+    };
     fetchCountry()
         .then(renderCountryCard)
         .catch(error => console.log(error));
@@ -37,8 +34,24 @@ function fetchCountry(){
 }
 
 function renderCountryCard(names) {
-    const markup = countryCardTpl(names);
-    refs.cardContainer.innerHTML = markup;
-    }
+    const countryList = document.querySelector('.country-list');
+    
+    // if (names.length < 11){
+    //     const markup = names
+    //         .map((name) => {
+    //             return `
+    //             <li class="row-list">
+    //                 <img src="${name.flag}" width="40" alt="${name.demonym}">
+    //                 <h1>${name.name}</h1>
+    //             </li>`;
+    //         })
+    //         .join("");
+    //     countryList.innerHTML = markup;
+    // }
+   
+        const markup = countryCardTpl(names);
+        refs.cardContainer.innerHTML = markup;
+        countryList.style.display = "none";
+    
 }
-
+}
